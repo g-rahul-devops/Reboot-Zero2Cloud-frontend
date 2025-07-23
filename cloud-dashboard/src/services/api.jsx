603591a1e271c47fbc737861dacbecff4a7a9523
Akhila001 <pavushettyakhila64@gmail.com>
2025-07-23 13:39:13 +0530

    productionize code

--- cloud-dashboard/src/services/api.jsx
+++ cloud-dashboard/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: 'http://localhost:9000',
     headers: {
         'Content-Type': 'application/json',
     },
